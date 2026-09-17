ddfd0a8c3130f92eed60dda966a26716119fb08a
emmanuellokudu <emmanuel.lokudu.f@gmail.com>
2026-09-17 16:31:27 +0300

    initial bill calculator commit

---
+++ bill_calc.py
@@ -0,0 +1,6 @@
+price = float(input("Enter the price of the item: "))
+quantity = int(input("Enter the quantity of the item: "))
+
+total_cost = price * quantity
+
+print(f"The total cost of the item is: KES.{total_cost:.2f}")

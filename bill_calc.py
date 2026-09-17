99fce717229af328cff88a47fd864f679fbf35f2
emmanuellokudu <emmanuel.lokudu.f@gmail.com>
2026-09-17 16:48:39 +0300

    add input validation

--- bill_calc.py
+++ bill_calc.py
@@ -1,6 +1,11 @@
-price = float(input("Enter the price of the item: "))
-quantity = int(input("Enter the quantity of the item: "))
 
-total_cost = price * quantity
+try:
 
-print(f"{quantity} items at KES {price:.2f} each = KES {total_cost:.2f}")
+  price = float(input("Enter the price of the item: "))
+  quantity = int(input("Enter the quantity of the item: "))
+
+  total_cost = price * quantity
+
+  print(f"{quantity} items at KES {price:.2f} each = KES {total_cost:.2f}")
+except ValueError:
+    print("Invalid input. Please enter a number for the price and a whole number for the quantity.")
